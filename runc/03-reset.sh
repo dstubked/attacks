173d3a499287fc9b1128cdfc496abb5b321a3791
dstubked <45514912+dstubked@users.noreply.github.com>
2019-07-15 18:43:36 +0800

    Update 03-reset.sh

--- runc/03-reset.sh
+++ runc/03-reset.sh
@@ -1,3 +1,4 @@
 #/bin/bash
-sudo apt-get remove -y docker-ce
-sudo apt-get install -y docker-ce=17.03.0~ce-0~ubuntu-xenial
+sudo apt-get -yq remove docker-ce > /dev/null
+sudo apt-get -yq install docker-ce=17.03.0~ce-0~ubuntu-xenial > /dev/null
+clear
