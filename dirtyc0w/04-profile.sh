650cbafecef5996842224cfa46a813573faad1f8
dstubked <45514912+dstubked@users.noreply.github.com>
2019-07-22 18:44:21 +0800

    Update 04-profile.sh

--- dirtyc0w/04-profile.sh
+++ dirtyc0w/04-profile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "docker run --rm -it -e AQUA_PROFILE=true bpdockerlab/dirtyc0w sh"
-docker run --rm -it -e AQUA_PROFILE=true bpdockerlab/dirtyc0w
+docker run --rm -it -e AQUA_PROFILE=true bpdockerlab/dirtyc0w sh
