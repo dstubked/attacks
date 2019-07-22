a4d0050c77cb6b8c40fd231ed21f1dcf20df4232
dstubked <45514912+dstubked@users.noreply.github.com>
2019-07-22 19:44:45 +0800

    Update 06-reset.sh

--- runc/06-reset.sh
+++ runc/06-reset.sh
@@ -2,5 +2,4 @@
 #sudo apt-get -yq remove docker-ce > /dev/null
 #sudo apt-get -yq install docker-ce=17.03.0~ce-0~ubuntu-xenial > /dev/null
 sudo cp -p /home/vagrant/docker-runc /usr/bin/docker-runc
-sudo /opt/aquasec/uninstall.sh
 clear
