a0392265cc562b216b8482aeddf690b8f6da9246
dstubked <45514912+dstubked@users.noreply.github.com>
2019-07-22 19:07:00 +0800

    Update 03-reset.sh

--- runc/03-reset.sh
+++ runc/03-reset.sh
@@ -1,4 +1,5 @@
 #/bin/bash
-sudo apt-get -yq remove docker-ce > /dev/null
-sudo apt-get -yq install docker-ce=17.03.0~ce-0~ubuntu-xenial > /dev/null
+#sudo apt-get -yq remove docker-ce > /dev/null
+#sudo apt-get -yq install docker-ce=17.03.0~ce-0~ubuntu-xenial > /dev/null
+sudo cp -p /home/vagrant/docker-runc /usr/bin/docker-runc
 clear
